cab5f4d9b636418518f6f2d66bcfe67e3a8359c6
LeoHou <sxs.leohou@gmail.com>
2023-02-26 16:01:57 +0800

    帳號功能 - 使用 MD5 加密密碼

--- src/main/java/com/leohou/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/leohou/springbootmall/service/impl/UserServiceImpl.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
 
 @Component
@@ -35,6 +36,10 @@ public class UserServiceImpl implements UserService {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
 		}
 
+//		使用 MD5 生成密碼的雜湊值
+		String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+		userRegisterRequest.setPassword(hashedPassword);
+
 //		創建帳號
 		return userDao.createUser(userRegisterRequest);
 	}
@@ -48,6 +53,11 @@ public class UserServiceImpl implements UserService {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
 		}
 
+//		使用 MD5 生成密碼的雜湊值
+		String hashedPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+		userLoginRequest.setPassword(hashedPassword);
+
+//		比較密碼
 		if (user.getPassword().equals(userLoginRequest.getPassword())) {
 			return user;
 		} else {
@@ -55,4 +65,5 @@ public class UserServiceImpl implements UserService {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
 		}
 	}
+
 }
